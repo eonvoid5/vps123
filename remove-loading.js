97bdfda7d331f8acb42392723bc7d72282f2dc7e
eonvoid5 <funk22586@gmail.com>
2026-08-30 16:43:38 +0530

    Remove startup loading screen

---
+++ remove-loading.js
@@ -0,0 +1,7 @@
+(()=>{
+'use strict';
+const css=document.createElement('style');
+css.id='void-remove-loading';
+css.textContent='.loading{display:none!important}';
+document.head.appendChild(css);
+})();
